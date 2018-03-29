7cbba14ccf925fc0bf4347eafcd48db8da96866d
Vidya <vidyajejurkar@gmail.com>
2018-03-29 21:29:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vidya/firebasepushexampleusingtopic/Constants.java
@@ -0,0 +1,12 @@
+package com.example.vidya.firebasepushexampleusingtopic;
+
+/**
+ * Created by Vidya on 3/29/2018.
+ */
+
+public class Constants {
+
+    public static final String CHANNEL_ID = "my_channel_01";
+    public static final String CHANNEL_NAME = "Firebase Notification using Topic";
+    public static final String CHANNEL_DESCRIPTION = "www.firebase.com";
+}
